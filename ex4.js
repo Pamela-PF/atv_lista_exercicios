abde437a51c691e01ce097065b96e905420a0bca
Pamela <ni.pan1010@gmail.com>
2022-04-16 20:15:48 -0300

    Atividade 4 completa

--- ex4.js
+++ ex4.js
@@ -22,4 +22,39 @@ let calcular = {
         }
     },
 }
+const calc = () => {
+    let opcao;
+    do{
+        console.log("****************** \n Menu - Escolha uma das opções: \n 1- Soma\n 2- Subtração \n 3- Multiplicação \n 4- Divisão \n 5- Sair");
+        opcao = prompt("Digite a opção escolhida: ");
 
+        switch (opcao){
+            case "1":
+                let aSo = parseInt(prompt("Digite o primeiro número: "));
+                let bSo = parseInt(prompt("Digite o segundo número: "));
+                console.log(calcular.soma(aSo, bSo));
+                break;
+            case "2":
+                let aSu = parseInt(prompt("Digite o primeiro número: "));
+                let bSu = parseInt(prompt("Digite o segundo número: "));
+                console.log(calcular.subtracao(aSu, bSu));
+                break;
+            case "3":
+                let aMu = parseInt(prompt("Digite o primeiro número: "));
+                let bMu = parseInt(prompt("Digite o segundo número: "));
+                console.log(calcular.multiplicacao(aMu, bMu));
+                break;
+            case "4":
+                let aDi = parseInt(prompt("Digite o primeiro número: "));
+                let bDi = parseInt(prompt("Digite o segundo número: "));
+                console.log(calcular.divisao(aDi, bDi));
+                break;
+            case "5":
+                console.log(`Você escolheu o número 5 que é o sair. Tchau :(`);
+                break;
+            default: 
+                console.log(`Opção invalida! Escolha os números entre 1 e 5.`);
+        }
+    }while (opcao !=5)
+}
+calc()
