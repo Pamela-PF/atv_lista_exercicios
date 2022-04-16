8a0ab8533d9cb7cabba4a86cdbe8cdbeff04e29b
Pamela <ni.pan1010@gmail.com>
2022-04-16 12:11:18 -0300

    Exercicio 4, adicionado o prompt-sync e as operacoes

--- ex4.js
+++ ex4.js
@@ -7,4 +7,19 @@
 O programa deve ficar em loop até que o usuário digite 5. Se o usuário digitar uma opção
 inválida, o programa deve informá-lo disso e mostrar o menu de opções novamente. Use
 o objeto JSON do exercício 4 para realizar as operações aritméticas.*/
+const prompt = require('prompt-sync')();
+
+let calcular = {
+    soma : (a, b) => a + b,
+    subtracao : (a, b) => a - b,
+    multiplicacao : (a, b) => a * b,
+    divisao : (a, b) => {
+        if (b==0){
+            return false;
+        }
+        else {
+            return a / b;
+        }
+    },
+}
 
